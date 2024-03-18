be50bde4d2bd15f34eb2ccb75648c5c2efe0d7b9
Peyman <peyman.osatian@gmail.com>
2024-03-18 22:09:36 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-03-18.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
