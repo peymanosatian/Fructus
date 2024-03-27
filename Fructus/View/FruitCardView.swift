310bbfe26a02eb3def57baa3b203b1ef1fd19a59
Peyman <peyman.osatian@gmail.com>
2024-03-27 23:23:21 +0100

    xcode18

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-03-27.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitCardView()
+}
---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-03-27.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartButtonView()
+}
