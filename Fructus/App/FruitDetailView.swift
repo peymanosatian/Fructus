f36a6ca76d1b4266376bad694ad8f7c56804b2d1
Peyman <peyman.osatian@gmail.com>
2024-04-12 00:34:26 +0200

    12april

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-04-11.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitDetailView()
+}
---
+++ Fructus/View/FruitHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-04-12.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitHeaderView()
+}
