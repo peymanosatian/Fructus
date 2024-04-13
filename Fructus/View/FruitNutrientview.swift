579db476cc658358a13067823250ab20b307a02c
Peyman <peyman.osatian@gmail.com>
2024-04-13 20:31:58 +0200

    13

---
+++ Fructus/View/FruitNutrientview.swift
@@ -0,0 +1,18 @@
+//
+//  FruitNutrientview.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-04-13.
+//
+
+import SwiftUI
+
+struct FruitNutrientview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitNutrientview()
+}
