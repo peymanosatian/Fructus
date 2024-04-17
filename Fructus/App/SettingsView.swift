1228b4bd25de44ec783b519c61f0ac0dbafcf895
Peyman <peyman.osatian@gmail.com>
2024-04-17 03:17:23 +0200

    17april

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-04-14.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
---
+++ Fructus/View/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  Fructus
+//
+//  Created by Peyman Osatian on 2024-04-14.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
